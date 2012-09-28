2b99384f10890228c79b658d018d6dc4c34ddfa3
Marek Stępniowski <marek@stepniowski.com>
2012-09-28 14:51:12 +0200

    Basic Twilio app.

--- hundredseconds/settings.py
+++ hundredseconds/settings.py
@@ -106,6 +106,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonehome',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
@@ -140,3 +141,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    import localsettings
+except ImportError:
+    pass
--- hundredseconds/urls.py
+++ hundredseconds/urls.py
@@ -8,10 +8,11 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hundredseconds.views.home', name='home'),
     # url(r'^hundredseconds/', include('hundredseconds.foo.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url('^phone/', include('phonehome.urls'))
 )
