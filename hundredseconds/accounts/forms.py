b52e6bd419b64937835f2cd820b61eac8e236cf8
Marek Stępniowski <marek@stepniowski.com>
2012-09-29 01:31:22 +0200

    Prevent user from adding wrongly formatted numbers to database.

--- hundredseconds/accounts/forms.py
+++ hundredseconds/accounts/forms.py
@@ -1,9 +1,17 @@
+import re
+
 from django import forms
+from django.forms import ValidationError
+
 from hundredseconds.accounts.models import User
 
 
 class UserPhoneForm(forms.ModelForm):
-
     class Meta:
         model = User
         fields = ('phone',)
+
+    def clean_phone(self):
+        if not re.match(r'[0-9]+', self.cleaned_data['phone']):
+            raise ValidationError('Phone number should contain only numbers.')
+        return self.cleaned_data['phone']
