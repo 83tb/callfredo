f0337172ebbb870b40c3347bc6b5f29390f31704
haxoza <prze.lewandowski@gmail.com>
2012-09-28 16:54:10 +0200

    Admin for new user model.

--- hundredseconds/accounts/admin.py
+++ hundredseconds/accounts/admin.py
@@ -1,11 +1,35 @@
 from django.contrib import admin
-from hundredseconds.accounts.models import User
 from django.contrib.auth.models import User as AuthUser
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import ugettext, ugettext_lazy as _
+from hundredseconds.accounts.models import User
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ("username",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = User
 
 
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'phone', 'email', 'first_name', 'last_name', 'last_login', 'date_joined', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
 
 
 admin.site.unregister(AuthUser)
