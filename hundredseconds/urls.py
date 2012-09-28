e348694cf076bb356eb9d4e563e7dc22d39f9381
haxoza <prze.lewandowski@gmail.com>
2012-09-28 15:09:31 +0200

    Few templates and settings for social auth.

--- hundredseconds/urls.py
+++ hundredseconds/urls.py
@@ -1,17 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from social_auth import urls as social_auth_urls
+from hundredseconds import views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'hundredseconds.views.home', name='home'),
-    # url(r'^hundredseconds/', include('hundredseconds.foo.urls')),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^social/', include(social_auth_urls)),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
