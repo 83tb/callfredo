a5ca6866ed4a059044dad74d736bad94fc1251ee
haxoza <prze.lewandowski@gmail.com>
2012-09-28 15:46:48 +0200

    Logout fix.

--- hundredseconds/accounts/urls.py
+++ hundredseconds/accounts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from hundredseconds.accounts import views
 
 urlpatterns = patterns('',
-    url(r'^logout/$', django.contrib.auth.views.logout, name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^social-error/$', views.SocialErrorView.as_view()),
 )
