2961a155a32ec0367aac86177b0d245fc138c6d6
Kuba Kucharski <kuba.kucharski@gmail.com>
2012-09-28 18:29:38 +0200

    taking data from fb

--- hundredseconds/views.py
+++ hundredseconds/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render_to_response
+from accounts.models import  import User
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -9,4 +11,42 @@ def PlayerView(request):
 
     code = request.GET['code']
     return render_to_response('player.html', {'code':code })
+
+
+
+from filter_fb_data import *
+from phonehome.models import Call
+from datetime import datetime
+
+
+def getData(events,bdays,inbox):
+
+    now = datetime.now()
+    data = {
+        'name' : "Kuba Kucharski",
+        'unread' : get_unread_count(inbox),
+        'events' : get_only_today_events(events),
+        'bdays' : get_today_bdays(bdays),
+        }
+
+
+    Call.create(fetched_date=now,data = str(data),user = User.objects.filter(id=1))
+
+
+
+
+
+"""
+
+{ 'name' : "Kuba Kucharski",
+'unread' : 0,
+'events' : ['openreaktor','rails'],
+'bdays' : 'name',
+}
+
+
+
+
+"""
+
 
