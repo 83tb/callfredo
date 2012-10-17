3c97eca2ca0883ba76da515f0b612e68ca868da7
Kuba Kucharski <kuba.kucharski@gmail.com>
2012-10-16 20:18:00 -0700

    login required

--- views.py
+++ views.py
@@ -14,6 +14,10 @@ class IndexView(TemplateView):
 from phonehome.sendsms import send, fetch_code
 
 
+from django.contrib.auth.decorators import login_required
+
+
+@login_required()
 def GiveNumberView(request):
 
     template_name = 'givenumber.html'
@@ -28,7 +32,7 @@ def GiveNumberView(request):
 
             code = fetch_code()
             obj = User.objects.get(id=request.user.id)
-            obj.code = code
+            obj.code = code.upper()
             obj.save()
             send(request.user.first_name, code,form['phone'].value())
 
@@ -40,6 +44,7 @@ def GiveNumberView(request):
         'form': form,
         })
 
+@login_required()
 def ConfirmNumberView(request):
 
     template_name = 'confirmnumber.html'
@@ -78,10 +83,11 @@ class SaveInContactsView(TemplateView):
     template_name = 'saveincontacts.html'
 """
 
+@login_required()
 def TryItView(request):
     return render(request, 'tryit.html', { 'phone': request.user.phone, })
 
-
+@login_required()
 def PlayerView(request, id=0):
     try:
         recording = Recording.objects.get(id=id)
@@ -93,7 +99,7 @@ def PlayerView(request, id=0):
     return direct_to_template(request, template='player.html',
         extra_context={'url':recording.url, 'caller':caller, 'id': recording.id, 'name': recording.fb_user_name})
 
-
+@login_required()
 def birthdays(request):
     data = request.user.create_call()
     return HttpResponse(data)
