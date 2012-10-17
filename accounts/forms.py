c0b4fe19bd012d773023d3dd14085525c5bf4031
Ola Sitarska <ola@sitarska.com>
2012-10-17 13:59:58 -0700

    Fixed code verification

--- accounts/forms.py
+++ accounts/forms.py
@@ -37,7 +37,7 @@ class ConfirmForm(forms.ModelForm):
         self.fields['code'].widget.attrs['class'] = 'text'
 
     def clean_code(self):
-        if self.code != self.cleaned_data['code']:
+        if self.fields['code'] != self.cleaned_data['code']:
             raise ValidationError('Code is incorrect.')
         return self.cleaned_data['code']
 
